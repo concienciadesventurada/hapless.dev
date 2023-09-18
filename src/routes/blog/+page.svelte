6c2c90f19dd9029274f9db8b853f0685976068ed
concienciadesventurada <porquemarx@gmail.com>
2023-09-17 21:22:01 -0600

    blog route bigger screens breakpoints

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,23 +9,23 @@
 	<title>{config.title} | Blog</title>
 </svelte:head>
 
-<section class="w-5/6 mx-auto text-justify">
-  <h1 class="text-4xl text-dorado underline text-center"><b>posts</b></h1>
+<section class="w-5/6 md:w-1/2 lg:w-2/5 xl:w-1/3 mx-auto text-justify">
+	<h1 class="text-4xl md:text-5xl text-dorado underline text-center pb-4"><b>posts</b></h1>
 	<ul>
 		{#each data.posts as post}
 			<li class="py-4">
 				<article class="py-6">
 					<a href={`/blog/${post.slug}`}>
-						<div class="flex justify-between items-baseline">
-							<h1 class="text-lg text-dorado">
+						<div class="flex justify-between gap-6 items-baseline">
+							<h1 class="text-lg md:text-2xl lg:text-3xl text-dorado">
 								<b>{post.title}</b>
 							</h1>
 							<p class="text-end">
 								{formatDate(post.date)}
 							</p>
 						</div>
-						<hr class="border-2 border-azulito my-4" />
-						<p class="text-sm">
+						<hr class="border border-azulito my-4" />
+						<p class="text-sm lg:text-lg">
 							{post.description === null ? "" : `${post.description}`}
 						</p>
 					</a>
