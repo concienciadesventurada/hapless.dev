a03b8aa6b947a0c8ee36013cc33830b3ab202421
concienciadesventurada <porquemarx@gmail.com>
2023-09-17 21:06:30 -0600

    blog css rewritten for small screens

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,32 +9,23 @@
 	<title>{config.title} | Blog</title>
 </svelte:head>
 
-<section
-	class="w-5/6 mx-auto my-24 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3 xl2:w-1/3"
->
-	<ul class="w-full">
-		<h1 class="text-5xl text-dorado underline text-center"><b>Blog</b></h1>
+<section class="w-5/6 mx-auto text-justify">
+  <h1 class="text-4xl text-dorado underline text-center"><b>posts</b></h1>
+	<ul>
 		{#each data.posts as post}
-			<li class="my-12">
-				<article class="pb-12">
+			<li class="py-4">
+				<article class="py-6">
 					<a href={`/blog/${post.slug}`}>
-						<div class="flex flex-col justify-between items-baseline">
-							<h1
-								class="text-3xl pb-2 text-dorado
-               sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl xl2:text-4xl"
-							>
+						<div class="flex justify-between items-baseline">
+							<h1 class="text-lg text-dorado">
 								<b>{post.title}</b>
 							</h1>
-							<p
-								class="text-sm sm:text-lg md:text-lg lg:text-lg xl:text-xl xl2:text-xl italic py-4"
-							>
+							<p class="text-end">
 								{formatDate(post.date)}
 							</p>
 						</div>
-						<hr class="border-b-2 border-azulito" />
-						<p
-							class="text-md py-4 sm:text-md md:text-lg lg:text-lg xl:text-xl xl2:text-xl"
-						>
+						<hr class="border-2 border-azulito my-4" />
+						<p class="text-sm">
 							{post.description === null ? "" : `${post.description}`}
 						</p>
 					</a>
